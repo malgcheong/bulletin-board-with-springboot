827b680768ebc9fe699106f72962d828339bb28b
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-06 01:50:06 +0900

    fix: public 접근 제어자 추가

--- src/main/java/com/swjungle/board/post/dto/CreatePostRequest.java
+++ src/main/java/com/swjungle/board/post/dto/CreatePostRequest.java
@@ -1,3 +1,3 @@
 package com.swjungle.board.post.dto;
 
-record CreatePostRequest(String title, String content, String link, String category, int score, String author, String password) {}
+public record CreatePostRequest(String title, String content, String link, String category, int score, String author, String password) {}
