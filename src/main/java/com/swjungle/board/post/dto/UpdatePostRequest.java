41ebbe6b63cbce46f792f34d9872adbdb727766f
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-07 00:06:02 +0900

    refactor: score 형식 변경 및 author 추가

--- src/main/java/com/swjungle/board/post/dto/UpdatePostRequest.java
+++ src/main/java/com/swjungle/board/post/dto/UpdatePostRequest.java
@@ -1,4 +1,4 @@
 package com.swjungle.board.post.dto;
 
-public record UpdatePostRequest(String title, String content, String link, String category, String score, String password) {
+public record UpdatePostRequest(String title, String content, String link, String category, int score, String author, String password) {
 }
