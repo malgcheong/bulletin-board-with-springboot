119f8f3d2bfba1eb92564d17a114b8f7d412e089
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-06 14:14:49 +0900

    fest: Post controller - Create 추가

---
+++ src/main/java/com/swjungle/board/post/controller/PostController.java
@@ -0,0 +1,28 @@
+package com.swjungle.board.post.controller;
+
+import com.swjungle.board.common.dto.EnvelopeResponse;
+import com.swjungle.board.post.dto.CreatePostRequest;
+import com.swjungle.board.post.dto.PostResponse;
+import com.swjungle.board.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<EnvelopeResponse<PostResponse>> createPost(@RequestBody CreatePostRequest createPostRequest) {
+        PostResponse postResponse = postService.createPost(createPostRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(EnvelopeResponse.success(postResponse));
+    }
+
+}
