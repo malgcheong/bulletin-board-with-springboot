1a925a9385ba83ec4198e65a9b2880f81a918ad2
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-09 16:54:56 +0900

    feat: MemberNotFoundException 추가

--- src/main/java/com/swjungle/board/common/advice/GlobalExceptionHandler.java
+++ src/main/java/com/swjungle/board/common/advice/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.swjungle.board.common.advice;
 
 import com.swjungle.board.common.code.CommonErrorCode;
 import com.swjungle.board.common.dto.EnvelopeResponse;
+import com.swjungle.board.member.exception.MemberNotFoundException;
 import com.swjungle.board.post.exception.InvalidPostRequestException;
 import com.swjungle.board.post.exception.PostNotFoundException;
 import org.springframework.http.ResponseEntity;
@@ -31,5 +32,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         List<String> errorMessages = ex.getErrors();
         return ResponseEntity.status(CommonErrorCode.BAD_REQUEST.getHttpStatus())
                 .body(EnvelopeResponse.error(CommonErrorCode.BAD_REQUEST, errorMessages));
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<EnvelopeResponse<String>> handleMemberNotFoundException(MemberNotFoundException ex) {
+        return ResponseEntity.status(CommonErrorCode.NO_ACCOUNT.getHttpStatus())
+                .body(EnvelopeResponse.error(CommonErrorCode.NO_ACCOUNT, ex.getMessage()));
     }
 }
---
+++ src/main/java/com/swjungle/board/member/exception/MemberNotFoundException.java
@@ -0,0 +1,7 @@
+package com.swjungle.board.member.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class MemberNotFoundException extends RuntimeException {
+}
