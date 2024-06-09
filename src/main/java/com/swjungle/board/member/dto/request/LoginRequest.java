f7916d339d57039d2f5f993965be268d67632861
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-09 15:54:57 +0900

    feat: member request dto 생성

---
+++ src/main/java/com/swjungle/board/member/dto/request/LoginRequest.java
@@ -0,0 +1,3 @@
+package com.swjungle.board.member.dto.request;
+
+public record LoginRequest(String username, String password){}
---
+++ src/main/java/com/swjungle/board/member/dto/request/SignupRequest.java
@@ -0,0 +1,3 @@
+package com.swjungle.board.member.dto.request;
+
+public record SignupRequest(String username, String password){}
