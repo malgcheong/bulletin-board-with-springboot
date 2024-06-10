037c4e3ea30f84846665e06c98e11f2caf1ee2b6
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-11 02:42:26 +0900

    feat: comment repository 생성

---
+++ src/main/java/com/swjungle/board/comment/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.swjungle.board.comment.repository;
+
+import com.swjungle.board.comment.entity.Comment;
+import com.swjungle.board.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
